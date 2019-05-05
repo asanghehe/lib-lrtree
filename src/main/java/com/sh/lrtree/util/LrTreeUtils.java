08f08f359b6d1de713f75195b09ee7c521ca5b9a
Linux_Ultimate <413820400@qq.com>
2019-05-05 17:38:30 +0800

    增加flat 扁平化方法

--- src/main/java/com/sh/lrtree/util/LrTreeUtils.java
+++ src/main/java/com/sh/lrtree/util/LrTreeUtils.java
@@ -23,21 +23,19 @@ public class LrTreeUtils {
 			
 			T item = list.get(i);
 
+			//数字类型的等于比较
 			if(item instanceof Number) {
 				if(item.getParentId() !=null && item.getParentId().equals(parentId)){
 					root.add(item);
 					list.remove(i);
 				}
-			}else if(item instanceof CharSequence){
-				String a = item.getParentId().toString();
-				String b = parentId.toString();
-				
-				if(item.getParentId() !=null && item.getParentId().equals(parentId)){
+			}else if(item instanceof CharSequence){		//字符串类型的等于 比较
+
+				if(item.getParentId() !=null && item.getParentId().toString().equals(parentId.toString())){
 					root.add(item);
 					list.remove(i);
 				}
 			}
-			
 		}
 		
 		Collections.reverse(root);
@@ -48,8 +46,7 @@ public class LrTreeUtils {
 				t.setChildren(buildListToSortedTree(list, t.getId()));
 			}
 		}
-		
-		
+
 		return root;
 	}
 	
@@ -107,6 +104,26 @@ public class LrTreeUtils {
 				
 				recursiveFillLevel(dept.getChildren(), parentLevel + 1);
 			}
+		}
+	}
+	
+	/**
+	 * 将树结构扁平化为列表
+	 */
+	public static <T extends LrBuildableTree<T, PK>, PK> void flatformTree(List<T> list) {
+		
+	}
+	
+	
+	/**
+	 * 将树结构扁平化为列表
+	 */
+	private static <T extends LrTree<PK>, PK> void flatformTreeInternal(List<T> src, List<T> target) {
+		for(T t: src) {
+			if(t.getChildren() != null) {
+				flatformTreeInternal(t.getChildren(), target);
+			}
+			target.add(t);
 		}
 	}
 }
